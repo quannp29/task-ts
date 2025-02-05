2b8caf35ced966000880a71de6073ccab31cd297
Nguyen Phu Quan <93207485+npquan29@users.noreply.github.com>
2025-02-05 23:17:03 +0700

    config connect db

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("OKE123");
+import express, { Express, Request, Response } from "express";
+import env from "dotenv";
+env.config();
+
+import { connect } from "./config/database";
+import Task from "./models/task.model";
+connect();
+
+const app: Express = express();
+const port: (number | string) = `${process.env.PORT}`;
+
+app.get("/tasks", async (req: Request, res: Response) => {
+  const tasks = await Task.find({
+    deleted: false
+  });
+
+  res.json(tasks);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
