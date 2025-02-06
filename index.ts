ddd35672043b3c2f192a3f4fd515a982be6eb7ff
Nguyen Phu Quan <93207485+npquan29@users.noreply.github.com>
2025-02-06 09:46:16 +0700

    get detail task

--- index.ts
+++ index.ts
@@ -9,13 +9,25 @@ connect();
 const app: Express = express();
 const port: (number | string) = `${process.env.PORT}`;
 
-app.get("/tasks", async (req: Request, res: Response) => {
+app.get("/tasks", async (req: Request, res: Response): Promise<void> => {
   const tasks = await Task.find({
     deleted: false
   });
 
   res.json(tasks);
 });
+
+app.get("/tasks/detail/:id", async (req: Request, res: Response): Promise<void> => {
+  const id: string = req.params.id;
+
+  const task = await Task.findOne({
+    _id: id,
+    deleted: false
+  });
+
+  res.json(task);
+});
+
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
