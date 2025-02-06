184a9f312764deadebbb5a24ecbab04eba0e34d7
Nguyen Phu Quan <93207485+npquan29@users.noreply.github.com>
2025-02-06 10:39:34 +0700

    filter by status task

--- v1/controllers/task.controller.ts
+++ v1/controllers/task.controller.ts
@@ -3,9 +3,20 @@ import Task from "../models/task.model";
 
 // [GET] /api/v1/tasks
 export const index = async (req: Request, res: Response): Promise<void> => {
-  const tasks = await Task.find({
+  interface Find {
+    deleted: false,
+    status?: string
+  }
+  
+  const find: Find = {
     deleted: false
-  });
+  };
+
+  if(req.query.status) {
+    find.status = `${req.query.status}`;
+  }
+
+  const tasks = await Task.find(find);
 
   res.json(tasks);
 }
